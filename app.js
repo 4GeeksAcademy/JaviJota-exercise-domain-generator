9219f5575a65003d31cddd981623127fd17997a8
JaviJota <165896246+JaviJota@users.noreply.github.com>
2024-05-31 07:12:43 +0000

    Creación de generador de dominios

---
+++ app.js
@@ -0,0 +1,27 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let domain = ['.com', '.net', '.es', '.org'];
+const domainContainer = document.querySelector('.domain-list');
+
+function getRandomDomain(){
+    let randomDomainsArr = []
+    for(let pronouns of pronoun){
+        let randomDomain = '';
+        for(let adjs of adj){
+            for(let nouns of noun){
+                for(let domains of domain){
+                    randomDomain = `${pronouns}${adjs}${nouns}${domains}`;
+                    if (randomDomainsArr.length === 0){
+                        randomDomainsArr.push(randomDomain);
+                    }
+                    else randomDomainsArr.push('<br>' + randomDomain);
+                }
+            }
+        }
+    }
+    return randomDomainsArr
+}
+domainContainer.innerHTML = getRandomDomain();
+
+console.log(getRandomDomain());
